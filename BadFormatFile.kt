ecb8196dae2fddc9369fc5bc6bd5fda8e9ad30ca
Brais Gabín <braisgabin@gmail.com>
2020-04-10 16:49:48 +0200

    asdf

--- BadFormatFile.kt
+++ BadFormatFile.kt
@@ -27,15 +27,11 @@ interface Hanabi {
 
 data class ActionPlay(val cardIndex: Int) : Hanabi.Action
 
-data class ActionDiscard(val cardIndex:Int) : Hanabi.Action
+data class ActionDiscard(val cardIndex: Int) : Hanabi.Action
 
-data class ActionHintColor(val player:Int,val color: Int
+data class ActionHintColor(
+    val player: Int,
+    val color: Int
 ) : Hanabi.Action
-
-
-
-
-
-
 
 data class ActionHintNumber(val player: Int, val number: Int) : Hanabi.Action
